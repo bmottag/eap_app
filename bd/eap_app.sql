7b3b0ae0420bb0ad8fb163f09cc08361375bfb46
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-03 19:49:38 -0600

    Se agrega tabla de proyectos y de horario de trabajo

--- bd/eap_app.sql
+++ bd/eap_app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-04-2018 a las 16:13:07
+-- Tiempo de generación: 04-04-2018 a las 03:39:42
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -37,7 +37,39 @@ CREATE TABLE `param_rol` (
 --
 
 INSERT INTO `param_rol` (`id_rol`, `rol_name`, `description`) VALUES
-(1, 'SUPER ADMIN', 'Con acceso a todo el sistema');
+(1, 'SUPER ADMIN', 'Con acceso a todo el sistema'),
+(2, 'FOREMAN', 'Encargado de revisar el payroll de los empleados'),
+(3, 'NORMAL USER', 'Empleados sin ningún privilegio');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `payroll`
+--
+
+CREATE TABLE `payroll` (
+  `id_payroll` int(10) NOT NULL,
+  `fk_id_user` int(10) NOT NULL,
+  `fk_id_project_start` int(10) NOT NULL,
+  `fk_id_project_finish` int(10) NOT NULL,
+  `start` datetime NOT NULL,
+  `finish` datetime NOT NULL,
+  `working_time` varchar(30) NOT NULL,
+  `working_hours` float NOT NULL,
+  `observation` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `project`
+--
+
+CREATE TABLE `project` (
+  `id_project` int(10) NOT NULL,
+  `project_name` varchar(200) NOT NULL,
+  `project_state` tinyint(1) NOT NULL COMMENT '1:Active; 2:inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -65,7 +97,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `first_name`, `last_name`, `log_user`, `email`, `fk_id_rol`, `birthdate`, `movil`, `password`, `state`, `photo`, `address`) VALUES
-(1, 'Benjamin', 'Motta', 'bmottag', 'benmotta@gmail.com', 1, '2018-03-19', '4033089921', '25d55ad283aa400af464c76d713c07ad', 1, '', '');
+(1, 'Benjamin', 'Motta', 'bmottag', 'benmotta@gmail.com', 1, '2018-03-19', '4033089921', '25f9e794323b453885f5181f1b624d0b', 1, '', '');
 
 --
 -- Índices para tablas volcadas
@@ -76,6 +108,21 @@ INSERT INTO `user` (`id_user`, `first_name`, `last_name`, `log_user`, `email`, `
 --
 ALTER TABLE `param_rol`
   ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `payroll`
+--
+ALTER TABLE `payroll`
+  ADD PRIMARY KEY (`id_payroll`),
+  ADD KEY `fk_id_user` (`fk_id_user`),
+  ADD KEY `fk_id_project_start` (`fk_id_project_start`),
+  ADD KEY `fk_id_project_finish` (`fk_id_project_finish`);
+
+--
+-- Indices de la tabla `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id_project`);
 
 --
 -- Indices de la tabla `user`
@@ -94,12 +141,33 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `param_rol`
 --
 ALTER TABLE `param_rol`
-  MODIFY `id_rol` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_rol` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `payroll`
+--
+ALTER TABLE `payroll`
+  MODIFY `id_payroll` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `project`
+--
+ALTER TABLE `project`
+  MODIFY `id_project` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `payroll`
+--
+ALTER TABLE `payroll`
+  ADD CONSTRAINT `payroll_ibfk_2` FOREIGN KEY (`fk_id_project_finish`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `payroll_ibfk_3` FOREIGN KEY (`fk_id_project_start`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
