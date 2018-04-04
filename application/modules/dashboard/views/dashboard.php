0838cb000c705221dadfddffec647686e7aa5367
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-04 14:46:31 -0600

    Ajuste de forma. Se coloca titulo

--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -2,7 +2,16 @@
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12">
 			<div class="x_panel">
+				<div class="x_title">
+					<h2><i class='fa fa-users'></i> LAST PAYROLL RECORDS  </h2>
+					
+					<ul class="nav navbar-right panel_toolbox">
+						<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+						</li>
 
+					</ul>
+					<div class="clearfix"></div>
+				</div>
 				<div class="x_content">
 								
 					<div class="table-responsive">
