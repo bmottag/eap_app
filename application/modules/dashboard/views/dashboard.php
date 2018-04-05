0fe6cb91ede75e5103fa8d8f3fc9ca9bf9710ea6
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-04 18:47:12 -0600

    Se valida si hay o no informacion para mostrar la tabla

--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -13,11 +13,13 @@
 					<div class="clearfix"></div>
 				</div>
 				<div class="x_content">
-								
+				<?php if(!$info){ ?>				
+					<div class="alert alert-success alert-dismissible fade in" role="alert">
+						<strong>Info:</strong> There is no payroll records.
+					</div>
+				<?php }else{ ?>			
 					<div class="table-responsive">
-					
 						<table id="datatable" class="table table-striped jambo_table bulk_action">
-
 							<thead>
 								<tr class="headings">
 								<th class="column-title">User </th>
@@ -31,25 +33,23 @@
 							</thead>
 
 							<tbody>
-										
-		<?php 
-			foreach ($info as $data):
-				echo "<tr>";
-				echo "<td>" . $data['employee'] . "</td>";
-				echo "<td class='text-center'>" . $data['start'] . "</td>";
-				echo "<td class='text-center'>" . $data['finish'] . "</td>";
-				echo "<td class='text-center'>" . $data['working_hours'] . "</td>";
-				echo "<td>" . $data['project_name'] . "</td>";
-				echo "<td>" . $data['observation'] . "</td>";
-				echo "<td>" . $data['activities'] . "</td>";
-				echo "</tr>";
-			endforeach 
-		?>
-
+							<?php 
+								foreach ($info as $data):
+									echo "<tr>";
+									echo "<td>" . $data['employee'] . "</td>";
+									echo "<td class='text-center'>" . $data['start'] . "</td>";
+									echo "<td class='text-center'>" . $data['finish'] . "</td>";
+									echo "<td class='text-center'>" . $data['working_hours'] . "</td>";
+									echo "<td>" . $data['project_name'] . "</td>";
+									echo "<td>" . $data['observation'] . "</td>";
+									echo "<td>" . $data['activities'] . "</td>";
+									echo "</tr>";
+								endforeach 
+							?>
 							</tbody>
 						</table>
 					</div>
-
+				<?php } ?>
 				</div>
 			</div>
 		</div>
