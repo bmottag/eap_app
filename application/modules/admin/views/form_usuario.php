fdff89f0f87711f04fa7a831b116f59d62815d4e
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-20 14:26:53 -0600

    Se coloco imagen a cada campo en el formulario de usuarios

--- application/modules/admin/views/form_usuario.php
+++ application/modules/admin/views/form_usuario.php
@@ -25,82 +25,93 @@
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="nombres">Firstname <span class="required">*</span>
 							</label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="nombres" name="nombres" required="required" class="form-control col-md-7 col-xs-12" value="<?php echo $information?$information[0]["first_name"]:""; ?>" maxlength=30 placeholder="Firstname">
+								<input type="text" id="nombres" name="nombres" required="required" class="form-control col-md-7 col-xs-12 has-feedback-left" value="<?php echo $information?$information[0]["first_name"]:""; ?>" maxlength=30 placeholder="Firstname">
+								<span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
+						
 						<div class="form-group">
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="apellidos">Lastname <span class="required">*</span>
 							</label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="apellidos" name="apellidos" required="required" class="form-control col-md-7 col-xs-12" value="<?php echo $information?$information[0]["last_name"]:""; ?>" maxlength=30 placeholder="Lastname">
+								<input type="text" id="apellidos" name="apellidos" required="required" class="form-control col-md-7 col-xs-12 has-feedback-left" value="<?php echo $information?$information[0]["last_name"]:""; ?>" maxlength=30 placeholder="Lastname">
+								<span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="usuario">User <span class="required">*</span>
 							</label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="usuario" name="usuario" required="required" class="form-control col-md-7 col-xs-12" value="<?php echo $information?$information[0]["log_user"]:""; ?>" maxlength=30 placeholder="User">
+								<input type="text" id="usuario" name="usuario" required="required" class="form-control col-md-7 col-xs-12 has-feedback-left" value="<?php echo $information?$information[0]["log_user"]:""; ?>" maxlength=30 placeholder="User">
+								<span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="email" class="control-label col-md-3 col-sm-3 col-xs-12">Email <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input id="email" name="email" class="form-control col-md-7 col-xs-12" type="text" value="<?php echo $information?$information[0]["email"]:""; ?>" maxlength=50 placeholder="Email">
+								<input id="email" name="email" class="form-control col-md-7 col-xs-12 has-feedback-left" type="text" value="<?php echo $information?$information[0]["email"]:""; ?>" maxlength=50 placeholder="Email">
+								<span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="celular" class="control-label col-md-3 col-sm-3 col-xs-12">Movil <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input id="celular" name="celular" class="form-control col-md-7 col-xs-12" type="text" value="<?php echo $information?$information[0]["movil"]:""; ?>" maxlength=12 placeholder="Movil">
+								<input id="celular" name="celular" class="form-control col-md-7 col-xs-12 has-feedback-left" type="text" value="<?php echo $information?$information[0]["movil"]:""; ?>" maxlength=12 placeholder="Movil">
+								<span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						
 						<div class="form-group">
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="type">User type <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<select name="type" id="type" class="form-control" >
+								<select name="type" id="type" class="form-control has-feedback-left" >
 									<option value=''>Select...</option>
 									<?php for ($i = 0; $i < count($userType); $i++) { ?>
 										<option value="<?php echo $userType[$i]["id_type"]; ?>" <?php if($information[0]["fk_id_type"] == $userType[$i]["id_type"]) { echo "selected"; }  ?>><?php echo $userType[$i]["user_type"]; ?></option>	
 									<?php } ?>
 								</select>
+								<span class="fa fa-bug form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						
 						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="hora_real">Hour value $ <span class="required">*</span></label>
+							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="hora_real">Hour value <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="hora_real" name="hora_real" class="form-control col-md-7 col-xs-12" value="<?php echo $information?$information[0]["hora_real"]:""; ?>" maxlength=5 placeholder="Hora real">
+								<input type="text" id="hora_real" name="hora_real" class="form-control col-md-7 col-xs-12 has-feedback-left" value="<?php echo $information?$information[0]["hora_real"]:""; ?>" maxlength=5 placeholder="Hora real">
+								<span class="fa fa-dollar form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						
 						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="hora_contrato">Hour contract value $ </label>
+							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="hora_contrato">Hour contract value </label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="hora_contrato" name="hora_contrato" class="form-control col-md-7 col-xs-12" value="<?php echo $information?$information[0]["hora_contrato"]:""; ?>" maxlength=5 placeholder="Hora contrato">
+								<input type="text" id="hora_contrato" name="hora_contrato" class="form-control col-md-7 col-xs-12 has-feedback-left" value="<?php echo $information?$information[0]["hora_contrato"]:""; ?>" maxlength=5 placeholder="Hora contrato">
+								<span class="fa fa-dollar form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						
 						<div class="form-group">
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="rol">Rol <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<select name="rol" id="rol" class="form-control" >
+								<select name="rol" id="rol" class="form-control has-feedback-left" >
 									<option value=''>Select...</option>
 									<?php for ($i = 0; $i < count($roles); $i++) { ?>
 										<option value="<?php echo $roles[$i]["id_rol"]; ?>" <?php if($information[0]["fk_id_rol"] == $roles[$i]["id_rol"]) { echo "selected"; }  ?>><?php echo $roles[$i]["rol_name"]; ?></option>	
 									<?php } ?>
 								</select>
+								<span class="fa fa-cogs form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 						
 						<div class="form-group">
 							<label for="estado" class="control-label col-md-3 col-sm-3 col-xs-12">State <span class="required">*</span></label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<select name="state" id="state" class="form-control" required>
+								<select name="state" id="state" class="form-control has-feedback-left" required>
 									<option value=''>Select...</option>
 									<option value=1 <?php if($information[0]["state"] == 1) { echo "selected"; }  ?>>Active</option>
 									<option value=2 <?php if($information[0]["state"] == 2) { echo "selected"; }  ?>>Inactive</option>
 								</select>
+								<span class="fa fa-flag form-control-feedback left" aria-hidden="true"></span>
 							</div>
 						</div>
 
