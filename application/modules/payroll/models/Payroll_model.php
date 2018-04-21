5661c1662c6cff049db3d6df94c7a990ea0ca475
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-04-21 12:49:37 -0600

    Se elimina el modulo de reporte se pasa esos metodos para el payroll

--- application/modules/payroll/models/Payroll_model.php
+++ application/modules/payroll/models/Payroll_model.php
@@ -116,6 +116,102 @@
 					return false;
 				}
 		}
+		
+	/*
+	 ************************
+	 ************************
+	 ** METODOS PARA EL FOREMAN
+	 ************************
+	 ************************
+	 */
+	 
+		/**
+		 * Update payroll hour
+		 * @since 2/2/2018
+		 */
+		public function savePayrollHour() 
+		{
+				$name = $this->session->userdata['name'];//nombre de usuario conectado
+				$idPayroll = $this->input->post('hddIdentificador');
+				$inicio = $this->input->post('hddInicio');
+				$fin = $this->input->post('hddFin');
+				
+				$moreInfo = "<strong>Changue hour by " . $name . ".</strong> <br>Before -> Start: " . $inicio . " <br>Before -> Finish: " . $fin;
+				$observation = $this->input->post('hddObservation') . "<br>********************<br>" . $moreInfo . "<br>" . $this->input->post('observation') . "<br>Date: " . date("Y-m-d G:i:s") . "<br>********************";
+
+				$fechaStart = $this->input->post('start_date');
+				$horaStart = $this->input->post('start_hour');
+				$minStart = $this->input->post('start_min');
+				$fechaFinish = $this->input->post('finish_date');
+				$horaFinish = $this->input->post('finish_hour');
+				$minFinish = $this->input->post('finish_min');
+				
+				$fechaStart = $fechaStart . " " . $horaStart . ":" . $minStart . ":00";
+				$fechaFinish = $fechaFinish . " " . $horaFinish . ":" . $minFinish . ":00"; 
+
+				$sql = "UPDATE payroll";
+				$sql.= " SET observation='$observation', finish =  '$fechaFinish', start='$fechaStart'";
+				$sql.= " WHERE id_payroll=$idPayroll";
+
+				$query = $this->db->query($sql);
+
+				if ($query) {
+					return true;
+				} else {
+					return false;
+				}
+
+		}
+	
+		/**
+		 * Add PAYROLL
+		 * @since 3/4/2018
+		 */
+		public function savePayrollAdvanced() 
+		{
+				$name = $this->session->userdata['name'];//nombre de usuario conectado
+				
+				$observation =  $this->security->xss_clean($this->input->post('observation'));
+				$observation =  addslashes($observation);
+				
+				$observation .= "<br>********************<br>";
+				$observation .= "<strong>Payrrol inserted by " . $name . ".</strong>";
+				$observation .= "<br>Date: " . date("Y-m-d G:i:s") . "<br>********************";
+				
+				$activities =  $this->security->xss_clean($this->input->post('activities'));
+				$activities =  addslashes($activities);
+				
+				$fechaStart = $this->input->post('start_date');
+				$fechaFinish = $this->input->post('finish_date');
+				
+				$horaStart = $this->input->post('hora_inicio');
+				$horaStart = date("H:i:s", strtotime($horaStart));
+				
+				$horaFinish = $this->input->post('hora_final');
+				$horaFinish = date("H:i:s", strtotime($horaFinish));
+				
+				$fechaStart = $fechaStart . " " . $horaStart;
+				$fechaFinish = $fechaFinish . " " . $horaFinish; 
+				
+				$data = array(
+					'fk_id_project' => $this->input->post('project'),
+					'fk_id_user' => $this->input->post('user'),
+					'start' => $fechaStart,
+					'finish' => $fechaFinish,
+					'activities' => $activities,
+					'observation' => $observation
+				);	
+				
+				$query = $this->db->insert('payroll', $data);
+				$idPayroll = $this->db->insert_id();
+
+				if ($query) {
+					return $idPayroll;
+				} else {
+					return false;
+				}
+		}
+
 
 
 		
